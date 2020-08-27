2537b321a349b2c9a067a07d0e1ce8d03ab3c383
s16717 <56193797+s16717@users.noreply.github.com>
2020-08-27 20:11:27 +0200

    Updates

---
+++ precache-manifest.de12b7e4b725cc02e6b2e684edcf9ea9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08fce3ae266c256376529c58317c2129",
+    "url": "/facedetection/index.html"
+  },
+  {
+    "revision": "f80d16468759ab96bd8c",
+    "url": "/facedetection/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "01094d6221013d0c564d",
+    "url": "/facedetection/static/css/main.991ab827.chunk.css"
+  },
+  {
+    "revision": "f80d16468759ab96bd8c",
+    "url": "/facedetection/static/js/2.d9e91af6.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facedetection/static/js/2.d9e91af6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01094d6221013d0c564d",
+    "url": "/facedetection/static/js/main.adfd3df8.chunk.js"
+  },
+  {
+    "revision": "99d29fa1a4ca556ebf26",
+    "url": "/facedetection/static/js/runtime-main.7cf075ce.js"
+  }
+]);
